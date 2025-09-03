a955cb914779aa8e04575bb3eaa6a52dc3a0ac55
DarknessCrow <rodrigoantonioemci2016@gmail.com>
2025-09-03 11:53:23 -0400

    saludo en java

---
+++ saludo.java
@@ -0,0 +1,9 @@
+public class saludo(){
+    public static void main(String[] args){
+        System.out.println("Hola Equipo 14!! :D");
+        System.out.println("1. Rodrigo Antonio Apaza Villanueva");
+        System.out.println("2. Grissel Noemi Tinta Lopez");
+        System.out.println("3. Delma Fernanda Choque Valencia");
+        System.out.println("4. Alejandro Andres Yujra Paye");
+    }
+}

0bb1f71eaff7dfbdec956a983ba2d2e779121fec
fernanda7v <delma1234fernanda@gmail.com>
2025-09-03 11:15:26 -0600

    Correcion de  operacion.py

---
+++ operacion.py
@@ -0,0 +1,11 @@
+
+
+print("------INTEGRANTES--------")
+print("- Alejandro Andres Yuja Paye")
+print("- Delma Fernanda Choque Valencia")
+print("- Grissel Noemi Tinta Lopez")
+print("- Apaza Villanueva Rodrigo Antonio")
+
+x = 10
+y = 20
+print("La suma de", x, "y", y, "es:", x + y)
